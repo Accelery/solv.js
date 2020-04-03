88e8c686a700c9b519d92c9ac16ee73eae6227af
Quentin <qbodart@gmail.com>
2020-04-03 08:56:13 +0200

    Placeholder test

--- src/index.ts
+++ src/index.ts
@@ -1,8 +1,9 @@
 export const solve = (
   func: (variable: number) => number,
-  goal: number,
-  maxIterations: number,
-  precision: number
+  goal: number = 0,
+  maxIterations: number = 100,
+  precision: number = 0.01,
+  guess: number = 0,
 ) => {
   return 0;
 };
---
+++ src/tests/solv.test.ts
@@ -0,0 +1,7 @@
+import { solve } from '../';
+
+test('It should not throw an error', () => {
+  solve((x) => {
+    return x;
+  });
+});
