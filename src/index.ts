8f53542f276534e7b0e7162b22aa80afa04e07a2
Christophe Querton <kertof@gmail.com>
2020-04-13 09:42:53 +0200

    cleaner

--- src/index.ts
+++ src/index.ts
@@ -1,29 +1,39 @@
+/**
+ *
+ *
+ * @param {(variable: number) => number} functionToSolveFor
+ * @param {number} [targetResult=0]
+ * @param {number} [initialGuess=0]
+ * @param {number} [precision=0.01]
+ * @param {number} [maxIterations=100]
+ * @returns
+ */
 export const solve = (
-  func: (variable: number) => number,
-  goal: number = 0,
-  guess: number = 0,
+  functionToSolveFor: (variable: number) => number,
+  targetResult: number = 0,
+  initialGuess: number = 0,
   precision: number = 0.01,
   maxIterations: number = 100,
 ) => {
-  let x0: number = guess;
+  let x0: number = initialGuess;
   let x1: number;
   let f_x0: number;
   let f_x1: number;
   let g_x0: number;
 
   // Check of guess is already close enough to the goal
-  f_x0 = func(x0);
-  if (Math.abs(goal - f_x0) < precision) {
+  f_x0 = functionToSolveFor(x0);
+  if (Math.abs(targetResult - f_x0) < precision) {
     return x0;
   }
 
   // Iterate to get closer to goal.
   for (let i = 0; i < maxIterations; i++) {
     // Compute value for next point in converging series
-    f_x0 = func(x0) - goal;
-    g_x0 = (func(x0 + f_x0) - goal - f_x0) / f_x0;
+    f_x0 = functionToSolveFor(x0) - targetResult;
+    g_x0 = (functionToSolveFor(x0 + f_x0) - targetResult - f_x0) / f_x0;
     x1 = x0 - f_x0 / g_x0;
-    f_x1 = func(x1) - goal;
+    f_x1 = functionToSolveFor(x1) - targetResult;
 
     // Check return value type
     if (isNaN(f_x1)) {
